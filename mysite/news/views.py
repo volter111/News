7c26bccac1c72088bed200eb4753b60e8b242d29
volter111 <krut.valeriy@gmail.com>
2021-10-07 13:04:54 -0700

    Fix form, save news to bd via created form

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import News, Category
 from .forms import NewsForm
 
@@ -29,7 +29,10 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
